1bad4c669d084ce3570e6d9901013c40b544ee4c
Brett Chaldecott <brett.chaldecott@gmail.com>
2018-01-22 03:31:07 +0000

    1) Made CLI command connect to sever https interface

--- src/programs/cli/main.cpp
+++ src/programs/cli/main.cpp
@@ -1,5 +1,14 @@
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/error.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 #include <boost/exception/exception.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 #include <boost/exception_ptr.hpp> 
@@ -9,10 +18,13 @@
 #include "keto/common/Exception.hpp"
 #include "keto/environment/EnvironmentManager.hpp"
 #include "keto/environment/Constants.hpp"
+#include "keto/ssl/RootCertificate.hpp"
 
 namespace ketoEnv = keto::environment;
 namespace ketoCommon = keto::common;
-
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
 
 boost::program_options::options_description generateOptionDescriptions() {
     boost::program_options::options_description optionDescripion;
@@ -54,6 +66,84 @@ int main(int argc, char** argv)
             return 0;
         }
         
+        // Check command line arguments.
+        if(argc != 4 && argc != 5)
+        {
+            std::cerr <<
+                "Usage: http-client-sync-ssl <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
+                "Example:\n" <<
+                "    http-client-sync-ssl www.example.com 443 /\n" <<
+                "    http-client-sync-ssl www.example.com 443 / 1.0\n" << 
+                std::endl << optionDescription << std::endl;
+            return EXIT_FAILURE;
+        }
+        auto const host = argv[1];
+        auto const port = argv[2];
+        auto const target = argv[3];
+        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
+
+        // The io_context is required for all I/O
+        boost::asio::io_context ioc;
+
+        // The SSL context is required, and holds certificates
+        ssl::context ctx{ssl::context::sslv23_client};
+
+        // This holds the root certificate used for verification
+        keto::ssl::load_root_certificates(ctx);
+
+        // These objects perform our I/O
+        tcp::resolver resolver{ioc};
+        ssl::stream<tcp::socket> stream{ioc, ctx};
+
+        // Set SNI Hostname (many hosts need this to handshake successfully)
+        if(! SSL_set_tlsext_host_name(stream.native_handle(), host))
+        {
+            boost::system::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
+            throw boost::system::system_error{ec};
+        }
+
+        // Look up the domain name
+        auto const results = resolver.resolve(host, port);
+
+        // Make the connection on the IP address we get from a lookup
+        boost::asio::connect(stream.next_layer(), results.begin(), results.end());
+
+        // Perform the SSL handshake
+        stream.handshake(ssl::stream_base::client);
+
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, target, version};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+        // Send the HTTP request to the remote host
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
+        boost::beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        // Write the message to standard out
+        std::cout << res << std::endl;
+
+        // Gracefully close the stream
+        boost::system::error_code ec;
+        stream.shutdown(ec);
+        if(ec == boost::asio::error::eof)
+        {
+            // Rationale:
+            // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+            ec.assign(0, ec.category());
+        }
+        if(ec)
+            throw boost::system::system_error{ec};
+
+        // If we get here then the connection is closed gracefully
         
         KETO_LOG_INFO << "CLI Executed";
     } catch (keto::common::Exception& ex) {
